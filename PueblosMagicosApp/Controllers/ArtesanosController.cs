63548e08541893993acb4b582f5abaf7e680151e
Crystian <ccramirez85@outlook.com>
2021-10-06 20:02:33 -0500

    Primera Modificación
    se movio la Carpeta PueblosMagicosApp --> PueblosMagios

---
+++ PueblosMagicosApp/Controllers/ArtesanosController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace PueblosMagicos.PueblosMagicosApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ArtesanosController : ControllerBase
+    {
+        
+    }
+}
